4754f78a31a30ce86732f2c34bc2a000b51327b6
Williams25 <william.souza.timao@hotmail.com>
2022-01-21 11:25:28 -0300

    criando pagina de repositorios

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,63 @@
-/*
- *
- * HomePage
- *
- */
+import React, { memo } from "react";
+import styled from "styled-components";
+import { Header } from "@buffetjs/custom";
+import { Table } from "@buffetjs/core";
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+const Wrapper = styled.div`
+  padding: 18px 30px;
+  p {
+    margin-top: 1rem;
+  }
+`;
+
+const headers = [
+  {
+    name: "Name",
+    value: "name",
+  },
+  {
+    name: "Description",
+    value: "description",
+  },
+  {
+    name: "Url",
+    value: "html_url",
+  },
+];
+
+const rows = [
+  {
+    name: "Landing-page",
+    description: "Code to the sales landing page",
+    html_url: "https://github.com/Williams25/landing-page-won",
+  },
+  {
+    name: "API Landing-page",
+    description: "Code to the sales landing page",
+    html_url: "https://github.com/Williams25/api-landing-page-won",
+  },
+  {
+    name: "Won Games",
+    description: "Code to the sales landing page",
+    html_url: "https://github.com/Williams25/won-games",
+  },
+  {
+    name: "API Won Games",
+    description: "Code to the sales landing page",
+    html_url: "https://github.com/Williams25/api-wongames",
+  },
+];
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: "Ract Avançado Repositories" }}
+        content="A list of our repositories in React Avançado courser."
+      />
+
+      <Table headers={headers} rows={rows}></Table>
+    </Wrapper>
   );
 };
 
