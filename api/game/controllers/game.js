6a25d34fccf0452bba7b3925299cc8902e9f817a
Williams25 <william.souza.timao@hotmail.com>
2022-02-17 19:24:26 -0300

    criando controller para game

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const populate = async (ctx) => {
+  await strapi.services.game.populate();
+  ctx.send("Finished pupulating!");
+};
 
-module.exports = {};
+module.exports = {
+  populate,
+};
